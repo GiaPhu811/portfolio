7594eedd4b20b279cd7eabf6190433b2f9b548d3
giaphu <phu.tran@redweb.dk>
2022-08-24 15:59:24 +0700

    Portfolio

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: {
+    esmExternals: true,
+    removeConsole: isProd,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
